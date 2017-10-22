2fc49a384f83e5f158af481a122a9f1b4cfa69cd
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-10-22 14:27:18 -0600

    City Food

---
+++ js/app.js
@@ -0,0 +1,60 @@
+$(function () {
+  'use strict';
+  var subtotal = 0;
+  var tax = 0;
+
+  $('.button-collapse').sideNav();
+
+  $('.card-action').click(function(e) {
+    e.preventDefault();
+
+    // create row with 3 cells
+    var tr = $('<tr>');
+    var td1 = $('<td>');
+
+    // set text of first cell to the title of the card clicked
+    td1.html($('span.card-title', e.target.parentNode.parentNode).html());
+    var td2 = $('<td>');
+    var td3 = $('<td>');
+
+    // set text of last cell to the price of the card clicked
+    td3.html($('p.price', e.target.parentNode.parentNode).html());
+
+    // append cells to the row.
+    $(tr).append(td1);
+    $(tr).append(td2);
+    $(tr).append(td3);
+
+    // append rows to the body of the table
+    $('#order-table tbody').append(tr); // .append($td);
+
+    var str = $(td3).html();
+    var res = str.substring(1, str.length);
+
+    subtotal += parseFloat(res);
+    var newSubtotal = subtotal.toFixed(2);
+
+    tax = parseFloat(res);
+
+    // total = parseFloat(res);
+    var total = (subtotal + tax).toFixed(2);
+
+    tax = (newSubtotal * 0.09).toFixed(2);
+
+    $('#subtotal').html(newSubtotal);
+    $('#tax').html(tax);
+    $('#total').html(total);
+
+    return;
+  });
+});
+
+$(function() {
+  'use strict';
+  $('#details-form').on('submit', function(e) {
+    var $toastContent = $('<span>Enjoy Your City Food!</span>');
+
+    Materialize.toast($toastContent, 5000);
+    e.preventDefault();
+  });
+});
