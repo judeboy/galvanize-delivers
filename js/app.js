a17f331e5ed50faff4eb72de9707dc6ae0f6a8ce
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-10-24 09:32:48 -0600

    changing colors

--- js/app.js
+++ js/app.js
@@ -8,25 +8,25 @@ $(function () {
   $('.card-action').click(function(e) {
     e.preventDefault();
 
-    // create row with 3 cells
+
     var tr = $('<tr>');
     var td1 = $('<td>');
 
-    // set text of first cell to the title of the card clicked
+
     td1.html($('span.card-title', e.target.parentNode.parentNode).html());
     var td2 = $('<td>');
     var td3 = $('<td>');
 
-    // set text of last cell to the price of the card clicked
+
     td3.html($('p.price', e.target.parentNode.parentNode).html());
 
-    // append cells to the row.
+
     $(tr).append(td1);
     $(tr).append(td2);
     $(tr).append(td3);
 
-    // append rows to the body of the table
-    $('#order-table tbody').append(tr); // .append($td);
+
+    $('#order-table tbody').append(tr);
 
     var str = $(td3).html();
     var res = str.substring(1, str.length);
@@ -36,7 +36,7 @@ $(function () {
 
     tax = parseFloat(res);
 
-    // total = parseFloat(res);
+
     var total = (subtotal + tax).toFixed(2);
 
     tax = (newSubtotal * 0.09).toFixed(2);
